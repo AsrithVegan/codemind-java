ae9523da723f90701cfe1bae90558a0b7eb53694
AsrithVegan <119785953+AsrithVegan@users.noreply.github.com>
2023-08-17 11:03:45 +0530

    import java.util.*; public class bigoftwo{     public static void main(String[] args){         int n;         Scanner se = new Scanner(System.in);         n = se.nextInt();         if(n%9==0){             System.out.print("True");         }         else{             System.out.print("False");         } } }

---
+++ Number_Divisible_with_9.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class bigoftwo{
+    public static void main(String[] args){
+        int n;
+        Scanner se = new Scanner(System.in);
+        n = se.nextInt(); 
+        if(n%9==0){
+            System.out.print("True");
+        }       
+        else{
+            System.out.print("False");
+        }
+}
+}
