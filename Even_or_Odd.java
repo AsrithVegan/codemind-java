1b0cc0e6ec3cfe408ee1bc83aef13b18d19a5b90
AsrithVegan <119785953+AsrithVegan@users.noreply.github.com>
2023-08-17 10:57:15 +0530

    import java.util.*; public class evorod{     public static void main(String[] args){         int n;         Scanner se = new Scanner(System.in);         n = se.nextInt();         if(n%2==0){             System.out.println("Even");         }         else{             System.out.println("Odd");         }     } }

---
+++ Even_or_Odd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class evorod{
+    public static void main(String[] args){
+        int n;
+        Scanner se = new Scanner(System.in);
+        n = se.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
