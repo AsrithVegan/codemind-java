82c375ea99d2754034163c9aa4d99ba3ced8deea
AsrithVegan <119785953+AsrithVegan@users.noreply.github.com>
2023-08-18 11:29:04 +0530

    import java.util.*; public class nnum{     public static void main(String[] args){         int i,n;         Scanner sc = new Scanner(System.in);         n = sc.nextInt();         for(i =1;i<=n;i++){             System.out.print(i+" ");         }     } }

---
+++ N_natural_numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class nnum{
+    public static void main(String[] args){
+        int i,n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for(i =1;i<=n;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
