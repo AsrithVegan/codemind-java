c5fc997850beab99cebb77d6cb7e630d74253346
AsrithVegan <119785953+AsrithVegan@users.noreply.github.com>
2023-08-18 11:33:42 +0530

    import java.util.*; public class sumofnum{     public static void main(String[] args){         int n,i;         int sum = 0;         Scanner sc = new Scanner(System.in);         n = sc.nextInt();         for (i=1;i<=n;i++){             sum+=i;         }         System.out.print(sum);     } }

---
+++ Sum_of_N_natural_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sumofnum{
+    public static void main(String[] args){
+        int n,i;
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (i=1;i<=n;i++){
+            sum+=i;
+        }
+        System.out.print(sum);
+    }
+}
