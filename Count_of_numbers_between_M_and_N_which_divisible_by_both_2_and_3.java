7a786f4a782afa9a6fcf6085fd745a1b2c313c49
AsrithVegan <119785953+AsrithVegan@users.noreply.github.com>
2023-08-18 12:09:50 +0530

    import java.util.*; public class twothreediv{     public static void main(String[] args){         int m,n,i;         int c=0;         Scanner sc = new Scanner(System.in);         m = sc.nextInt();         n = sc.nextInt();         for (i=m;i<=n;i++){             if(i%2==0&&i%3==0){                c++;             }         }         System.out.print(c);     } }

---
+++ Count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class twothreediv{
+    public static void main(String[] args){
+        int m,n,i;
+        int c=0;
+        Scanner sc = new Scanner(System.in);
+        m = sc.nextInt();
+        n = sc.nextInt();
+        for (i=m;i<=n;i++){
+            if(i%2==0&&i%3==0){
+               c++;
+            }
+        }
+        System.out.print(c);
+    }
+}
