c8317c9174e1ea57f9930070291ad893a002846d
AsrithVegan <119785953+AsrithVegan@users.noreply.github.com>
2023-08-26 09:59:58 +0530

    import java.util.*; public class randj{     public static void main(String[] args){         int x,y,z,a;         Scanner sc = new Scanner(System.in);         x = sc.nextInt();         y = sc.nextInt();         z = sc.nextInt();         a = 5*x+y*10;         System.out.print(a/z);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class randj{
+    public static void main(String[] args){
+        int x,y,z,a;
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        y = sc.nextInt();
+        z = sc.nextInt();
+        a = 5*x+y*10;
+        System.out.print(a/z);
+    }
+}
