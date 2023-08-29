869ae7d241e4b107d851751b5023ff067ed50330
AsrithVegan <119785953+AsrithVegan@users.noreply.github.com>
2023-08-29 19:03:11 +0530

    import java.util.*; public class ItoC{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         double n = sc.nextDouble();         double cm = n*2.54;         System.out.printf("%.2f",cm);     } }

---
+++ inches_to_centimeters.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ItoC{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        double cm = n*2.54;
+        System.out.printf("%.2f",cm);
+    }
+}
