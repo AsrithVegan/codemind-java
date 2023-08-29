bd2a2d6b36e378171a9b5e7eca8b318bb42a0c64
AsrithVegan <119785953+AsrithVegan@users.noreply.github.com>
2023-08-29 19:00:02 +0530

    import java.util.*; public class CI{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         double p = sc.nextDouble();         double r = sc.nextDouble();         double t = sc.nextDouble();         double ci=p*(Math.pow((1 + r / 100), t))-p;         System.out.printf("%.2f",ci);     } }

---
+++ Compound_Interest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CI{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double p = sc.nextDouble();
+        double r = sc.nextDouble();
+        double t = sc.nextDouble();
+        double ci=p*(Math.pow((1 + r / 100), t))-p;
+        System.out.printf("%.2f",ci);
+    }
+}
