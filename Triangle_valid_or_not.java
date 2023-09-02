a1467760395514698d9e4cb7d4dc15340ce345b4
AsrithVegan <119785953+AsrithVegan@users.noreply.github.com>
2023-09-02 09:35:40 +0530

    import java.util.*; public class TriangleOrNot{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         int c = sc.nextInt();         if(a+b>c){             System.out.print("Valid triangle");         }         else{             System.out.print("Invalid triangle");         }     } }

---
+++ Triangle_valid_or_not.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class TriangleOrNot{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if(a+b>c){
+            System.out.print("Valid triangle");
+        }
+        else{
+            System.out.print("Invalid triangle");
+        }
+    }
+}
